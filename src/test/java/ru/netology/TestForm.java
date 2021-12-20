0f3e6a7a33b9415ea88d077ac6ca1577fcd755be
Aleksei Milenko <amylenko@mail.ru>
2021-12-20 14:41:30 +0300

    Tests added

--- src/test/java/ru/netology/TestForm.java
+++ src/test/java/ru/netology/TestForm.java
@@ -16,42 +16,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestForm {
 
-        private WebDriver driver;
+    private WebDriver driver;
 
-        @BeforeAll
-        public static void setUpAll() {
-            WebDriverManager.chromedriver().setup();
-        }
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-        @BeforeEach
-        public void setUp() {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--headless");
-            driver = new ChromeDriver(options);
-        }
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
 
-        @AfterEach
-        public void tearDown() {
-            driver.quit();
-            driver = null;
-        }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
 
     @Test
-            public void shouldSendForm()  {
-            driver.get("http://localhost:9999");
-            driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей М");
-            driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79211234567");
-            driver.findElement(By.cssSelector(".checkbox__box")).click();
-            driver.findElement(By.cssSelector("button")).click();
-            String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-            String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-            assertEquals (expected,actualText);
+    public void shouldSendForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей М");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79211234567");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualText);
 
-        }
+    }
 
-        @Test
+    @Test
     public void shouldNotSendNoName() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
@@ -60,7 +60,29 @@ public class TestForm {
         driver.findElement(By.cssSelector("button")).click();
         String actualText = driver.findElement(By.cssSelector("[class=\"input__sub\"]")).getText().trim();
         String expected = "Поле обязательно для заполнения";
-        assertEquals (expected,actualText);
-
+        assertEquals(expected, actualText);
     }
-}
+
+    @Test
+    public void shouldNotSendWrongTel() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей М");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7921123456");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualText = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, actualText);
+    }
+
+
+        @Test
+        public void shouldNotSendWrongTeld () {
+            driver.get("http://localhost:9999");
+            driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Алексей М");
+            driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79211234567");
+//            driver.findElement(By.cssSelector(".checkbox__box")).click();
+            driver.findElement(By.cssSelector("button")).click();
+            driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).getText().trim();
+        }
+    }
